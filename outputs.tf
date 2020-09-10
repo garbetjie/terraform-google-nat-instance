d775d9c77089022f787ff45ace1f8b3f8965ede9
Geoff Garbers <geoff@garbers.co.za>
2020-09-10 11:46:15 +0200

    Add descriptions to outputs and variables.

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output address {
   value = google_compute_address.address.address
+  description = "Internal IP address of this NAT instance."
 }
 
 output nat_address {
   value = var.address
+  description = "NAT IP address of this NAT instance."
 }
 
 output instance_name {
   value = google_compute_instance.instance.name
+  description = "Name of the Compute Engine instance."
 }
 
 output route_name {
   value = google_compute_route.route.name
+  description = "Name of the route used to route traffic through the instance."
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable address {
   type = string
+  description = "External IP address to assign to this instance."
 }
 
 variable zone {
   type = string
+  description = "Zone in which to place this instance. Must be the same region as the IP address provided."
 }
 
 variable machine_type {
   type = string
   default = "f1-micro"
+  description = "Machine type of the instance."
 }
 
 variable disk_size {
   type = number
   default = 15
+  description = "Size of the instance's disk (in GB)."
 }
 
 variable disk_type {
   type = string
   default = "pd-balanced"
+  description = "Type of the instance's disk (one of `pd-standard`, `pd-balanced` or `pd-ssd`)."
 }
 
 locals {
